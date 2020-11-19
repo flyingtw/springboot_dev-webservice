187cca9f15f899879750e547208f7f9f02f50d0f
flyingtw <flyingtw0823@gmail.com>
2020-11-19 15:49:21 +0900

    게시글 등록 - 랜덤 포트를 이용해 게시글 등록 - 테스트 코드 ok

---
+++ src/main/java/com/flyingtw/book/springboot/service/PostService.java
@@ -0,0 +1,22 @@
+package com.flyingtw.book.springboot.service;
+
+
+import com.flyingtw.book.springboot.domain.posts.PostsRepository;
+import com.flyingtw.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+
+}
---
+++ src/main/java/com/flyingtw/book/springboot/web/PostsApiController.java
@@ -0,0 +1,20 @@
+package com.flyingtw.book.springboot.web;
+
+import com.flyingtw.book.springboot.service.PostService;
+import com.flyingtw.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+        return postsService.save(requestDto);
+    }
+}
