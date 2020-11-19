039dc41ef09c2ad3b94366c47e15ece53d818180
flyingtw <flyingtw0823@gmail.com>
2020-11-19 17:28:20 +0900

    JPA Auditing 반영 - DB의 등록/수정 시간을 자동화함 - BaseTimeEntity를 상속받아 사용

--- src/main/java/com/flyingtw/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/flyingtw/book/springboot/domain/posts/Posts.java
@@ -1,6 +1,7 @@
 package com.flyingtw.book.springboot.domain.posts;
 
 
+import com.flyingtw.book.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,7 +12,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts  {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,7 +27,7 @@ public class Posts  {
     private String author;
 
     @Builder
-    public Posts(String title, String content, String author){
+    public Posts (String title, String content, String author){
         this.title=title;
         this.content=content;
         this.author=author;
